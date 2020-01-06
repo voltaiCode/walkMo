9f231040716fa210c86495d2aa8bb1a24d9d2ca1
phonores <55716410+phonores@users.noreply.github.com>
2020-01-06 04:00:08 -0800

    working map that toggles depending on the state

--- client/components/RenderMap.jsx
+++ client/components/RenderMap.jsx
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
-import { 
-  GoogleMap, 
-  withGoogleMap, 
-  DirectionsRenderer 
-} from 'react-google-maps';
+import { BrowserRouter , Route, Switch} from 'react-router-dom';
+import { GoogleMap, withGoogleMap, DirectionsRenderer } from 'react-google-maps';
 
 /**
  * RenderMap handles the map logic  
@@ -14,18 +11,19 @@ class RenderMap extends Component {
     super(props);
     this.state = {
       directions: null,
+      origin: { lat: 33.988010, lng: -118.470947 },
+      destination: { lat: 33.986961, lng: -118.472458 },
+      selectDestination: false,
     };
   }
-
+  
   componentDidMount() {
     const directionsService = new google.maps.DirectionsService();
-    const origin = { lat: 33.988010, lng: -118.470947 };
-    const destination = { lat: 33.986961, lng: -118.472458 };
 
     directionsService.route(
       {
-        origin: origin,
-        destination: destination,
+        origin: this.state.origin,
+        destination: this.state.destination,
         travelMode: google.maps.TravelMode.WALKING
       },
       (result, status) => {
@@ -43,23 +41,40 @@ class RenderMap extends Component {
 
   render() {
     // Wrapping the map with the correct funtions
-    const WrappedMap = withGoogleMap(props => (
+    const MapWithRoute = withGoogleMap(props => (
       // Setting the map component with props to be render
       <GoogleMap 
         defaultZoom={10} 
-        defaultCenter={{ lat: 33.988010, lng: -118.470947 }}
+        defaultCenter={this.state.origin}
       >
         <DirectionsRenderer
           directions={this.state.directions}
         />
       </GoogleMap>
     ));
+
+    // Wrapping the map with the correct funtions
+    const Map = withGoogleMap(props => (
+      // Setting the map component with props to be render
+      <GoogleMap 
+        defaultZoom={10} 
+        defaultCenter={this.state.origin}
+      />
+    ));
+
     return (
       <React.Fragment>
-        <WrappedMap 
-          containerElement={<div style={{ height: `100%`, width: "100%" }} />}
-          mapElement={<div style={{ height: `100%` }} />}
-        />       
+        {this.state.selectDestination ? 
+           <MapWithRoute
+            containerElement={<div style={{ height: `100%`, width: "100%" }} />}
+            mapElement={<div style={{ height: `100%` }} />}
+          />    
+          : 
+          <Map 
+            containerElement={<div style={{ height: `100%`, width: "100%" }} />}
+            mapElement={<div style={{ height: `100%` }} />}
+          />
+        }
       </React.Fragment>
     );
   }
